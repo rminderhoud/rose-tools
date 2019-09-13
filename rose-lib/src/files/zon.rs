4994b68b2cc95bd62237c93f6d77ccf2ca86fcd0
Ralph Minderhoud <ralphminderhoud@gmail.com>
2019-09-13 18:04:35 +0500

    Reorganize structs/enums in zon file

--- rose-lib/src/files/zon.rs
+++ rose-lib/src/files/zon.rs
@@ -10,137 +10,6 @@ use utils::{Vector2, Vector3};
 /// Zone File
 pub type ZON = Zone;
 
-/// Zone Type
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-pub enum ZoneType {
-    Grass = 0,
-    Mountain = 1,
-    MountainVillage = 2,
-    BoatVillage = 3,
-    Login = 4,
-    MountainGorge = 5,
-    Beach = 6,
-    JunonDungeon = 7,
-    LunaSnow = 8,
-    Birth = 9,
-    JunonField = 10,
-    LunaDungeon = 11,
-    EldeonField = 12,
-    EldeonField2 = 13,
-    JunonPyramids = 14,
-}
-
-impl TryFrom<i32> for ZoneType {
-    type Error = failure::Error;
-
-    fn try_from(value: i32) -> Result<Self, Self::Error> {
-        match value {
-            0 => Ok(ZoneType::Grass),
-            1 => Ok(ZoneType::Mountain),
-            2 => Ok(ZoneType::MountainVillage),
-            3 => Ok(ZoneType::BoatVillage),
-            4 => Ok(ZoneType::Login),
-            5 => Ok(ZoneType::MountainGorge),
-            6 => Ok(ZoneType::Beach),
-            7 => Ok(ZoneType::JunonDungeon),
-            8 => Ok(ZoneType::LunaSnow),
-            9 => Ok(ZoneType::Birth),
-            10 => Ok(ZoneType::JunonField),
-            11 => Ok(ZoneType::LunaDungeon),
-            12 => Ok(ZoneType::EldeonField),
-            13 => Ok(ZoneType::EldeonField2),
-            14 => Ok(ZoneType::JunonPyramids),
-            _ => {
-                bail!("Invalid ZoneType: {}", value);
-            }
-        }
-    }
-}
-
-/// Zone Block Type
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-pub enum ZoneBlockType {
-    BasicInfo = 0,
-    EventPoints = 1,
-    Textures = 2,
-    Tiles = 3,
-    Economy = 4,
-}
-
-impl TryFrom<i32> for ZoneBlockType {
-    type Error = failure::Error;
-
-    fn try_from(value: i32) -> Result<Self, Self::Error> {
-        match value {
-            0 => Ok(ZoneBlockType::BasicInfo),
-            1 => Ok(ZoneBlockType::EventPoints),
-            2 => Ok(ZoneBlockType::Textures),
-            3 => Ok(ZoneBlockType::Tiles),
-            4 => Ok(ZoneBlockType::Economy),
-            _ => {
-                bail!("Invalid ZoneBlockType: {}", value);
-            }
-        }
-    }
-}
-
-/// Zone Tile Rotation
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-pub enum ZoneTileRotation {
-    Unknown = 0,
-    None = 1,
-    FlipHorizontal = 2,
-    FlipVertical = 3,
-    Flip = 4,
-    Clockwise90 = 5,
-    CounterClockwise90 = 6,
-}
-
-impl TryFrom<i32> for ZoneTileRotation {
-    type Error = failure::Error;
-
-    fn try_from(value: i32) -> Result<Self, Self::Error> {
-        match value {
-            0 => Ok(ZoneTileRotation::Unknown),
-            1 => Ok(ZoneTileRotation::None),
-            2 => Ok(ZoneTileRotation::FlipHorizontal),
-            3 => Ok(ZoneTileRotation::FlipVertical),
-            4 => Ok(ZoneTileRotation::Flip),
-            5 => Ok(ZoneTileRotation::Clockwise90),
-            6 => Ok(ZoneTileRotation::CounterClockwise90),
-            _ => {
-                bail!("Invalid ZoneTileRotation: {}", value);
-            }
-        }
-    }
-}
-
-/// Zone Position
-#[derive(Clone, Copy, Debug, Serialize, Deserialize)]
-pub struct ZonePosition {
-    pub position: Vector2<f32>,
-    pub is_used: bool,
-}
-
-/// Zone Event Position
-#[derive(Debug, Serialize, Deserialize)]
-pub struct ZoneEventPoint {
-    pub position: Vector3<f32>,
-    pub name: String,
-}
-
-/// ZoneTile
-#[derive(Debug, Serialize, Deserialize)]
-pub struct ZoneTile {
-    pub layer1: i32,
-    pub layer2: i32,
-    pub offset1: i32,
-    pub offset2: i32,
-    pub blend: bool,
-    pub rotation: ZoneTileRotation,
-    pub tile_type: i32,
-}
-
 /// Zone
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Zone {
@@ -172,37 +41,6 @@ pub struct Zone {
     pub food_consumption: i32,
 }
 
-impl ZonePosition {
-    fn new() -> ZonePosition {
-        ZonePosition {
-            position: Vector2::<f32>::new(),
-            is_used: false,
-        }
-    }
-}
-
-impl ZoneEventPoint {
-    fn new() -> ZoneEventPoint {
-        ZoneEventPoint {
-            position: Vector3::<f32>::new(),
-            name: String::new(),
-        }
-    }
-}
-
-impl ZoneTile {
-    fn new() -> ZoneTile {
-        ZoneTile {
-            layer1: -1,
-            layer2: -1,
-            offset1: -1,
-            offset2: -1,
-            blend: false,
-            rotation: ZoneTileRotation::None,
-            tile_type: -1,
-        }
-    }
-}
 impl RoseFile for Zone {
     fn new() -> Zone {
         Zone {
@@ -332,5 +170,168 @@ impl RoseFile for Zone {
 
     fn write<W: WriteRoseExt>(&mut self, _writer: &mut W) -> Result<(), Error> {
         unimplemented!();
+    }
+}
+
+/// Zone Type
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub enum ZoneType {
+    Grass = 0,
+    Mountain = 1,
+    MountainVillage = 2,
+    BoatVillage = 3,
+    Login = 4,
+    MountainGorge = 5,
+    Beach = 6,
+    JunonDungeon = 7,
+    LunaSnow = 8,
+    Birth = 9,
+    JunonField = 10,
+    LunaDungeon = 11,
+    EldeonField = 12,
+    EldeonField2 = 13,
+    JunonPyramids = 14,
+}
+
+impl TryFrom<i32> for ZoneType {
+    type Error = failure::Error;
+
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(ZoneType::Grass),
+            1 => Ok(ZoneType::Mountain),
+            2 => Ok(ZoneType::MountainVillage),
+            3 => Ok(ZoneType::BoatVillage),
+            4 => Ok(ZoneType::Login),
+            5 => Ok(ZoneType::MountainGorge),
+            6 => Ok(ZoneType::Beach),
+            7 => Ok(ZoneType::JunonDungeon),
+            8 => Ok(ZoneType::LunaSnow),
+            9 => Ok(ZoneType::Birth),
+            10 => Ok(ZoneType::JunonField),
+            11 => Ok(ZoneType::LunaDungeon),
+            12 => Ok(ZoneType::EldeonField),
+            13 => Ok(ZoneType::EldeonField2),
+            14 => Ok(ZoneType::JunonPyramids),
+            _ => {
+                bail!("Invalid ZoneType: {}", value);
+            }
+        }
+    }
+}
+
+/// Zone Block Type
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub enum ZoneBlockType {
+    BasicInfo = 0,
+    EventPoints = 1,
+    Textures = 2,
+    Tiles = 3,
+    Economy = 4,
+}
+
+impl TryFrom<i32> for ZoneBlockType {
+    type Error = failure::Error;
+
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(ZoneBlockType::BasicInfo),
+            1 => Ok(ZoneBlockType::EventPoints),
+            2 => Ok(ZoneBlockType::Textures),
+            3 => Ok(ZoneBlockType::Tiles),
+            4 => Ok(ZoneBlockType::Economy),
+            _ => {
+                bail!("Invalid ZoneBlockType: {}", value);
+            }
+        }
+    }
+}
+
+/// Zone Position
+#[derive(Clone, Copy, Debug, Serialize, Deserialize)]
+pub struct ZonePosition {
+    pub position: Vector2<f32>,
+    pub is_used: bool,
+}
+
+impl ZonePosition {
+    fn new() -> ZonePosition {
+        ZonePosition {
+            position: Vector2::<f32>::new(),
+            is_used: false,
+        }
+    }
+}
+
+/// Zone Event Position
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ZoneEventPoint {
+    pub position: Vector3<f32>,
+    pub name: String,
+}
+
+impl ZoneEventPoint {
+    fn new() -> ZoneEventPoint {
+        ZoneEventPoint {
+            position: Vector3::<f32>::new(),
+            name: String::new(),
+        }
+    }
+}
+
+/// ZoneTile
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ZoneTile {
+    pub layer1: i32,
+    pub layer2: i32,
+    pub offset1: i32,
+    pub offset2: i32,
+    pub blend: bool,
+    pub rotation: ZoneTileRotation,
+    pub tile_type: i32,
+}
+
+impl ZoneTile {
+    fn new() -> ZoneTile {
+        ZoneTile {
+            layer1: -1,
+            layer2: -1,
+            offset1: -1,
+            offset2: -1,
+            blend: false,
+            rotation: ZoneTileRotation::None,
+            tile_type: -1,
+        }
+    }
+}
+
+/// Zone Tile Rotation
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub enum ZoneTileRotation {
+    Unknown = 0,
+    None = 1,
+    FlipHorizontal = 2,
+    FlipVertical = 3,
+    Flip = 4,
+    Clockwise90 = 5,
+    CounterClockwise90 = 6,
+}
+
+impl TryFrom<i32> for ZoneTileRotation {
+    type Error = failure::Error;
+
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(ZoneTileRotation::Unknown),
+            1 => Ok(ZoneTileRotation::None),
+            2 => Ok(ZoneTileRotation::FlipHorizontal),
+            3 => Ok(ZoneTileRotation::FlipVertical),
+            4 => Ok(ZoneTileRotation::Flip),
+            5 => Ok(ZoneTileRotation::Clockwise90),
+            6 => Ok(ZoneTileRotation::CounterClockwise90),
+            _ => {
+                bail!("Invalid ZoneTileRotation: {}", value);
+            }
+        }
     }
 }
